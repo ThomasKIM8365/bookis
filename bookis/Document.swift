01e1ce5d595c7a7bcbc4e31bf4250e41a7d3c78d
ThomasKIM8365 <lefteye4me@gmail.com>
2024-06-16 15:42:46 +0900

    Initial Commit

---
+++ bookis/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  bookis
+//
+//  Created by 김진규 on 6/16/24.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ bookis/DocumentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DocumentViewController.swift
+//  bookis
+//
+//  Created by 김진규 on 6/16/24.
+//
+
+import UIKit
+
+class DocumentViewController: UIDocumentViewController {
+
+    @IBOutlet weak var documentNameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.updateViewsIfNecessary()
+    }
+
+    override func documentDidOpen() {
+        super.documentDidOpen()
+        self.updateViewsIfNecessary()
+    }
+
+    func updateViewsIfNecessary() {
+        // Check if the document is open and the view is loaded
+        guard let document, !document.documentState.contains(.closed) else { return }
+        guard isViewLoaded else { return }
+
+        // Display the content of the document, e.g.:
+        self.documentNameLabel.text = document.localizedName
+    }
+
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true)
+    }
+}
